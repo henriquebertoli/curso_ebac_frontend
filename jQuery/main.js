c8876f771e1247f783addb5bdf3ac2b70d9576ec
henriquebertoli <135612565+henriquebertoli@users.noreply.github.com>
2023-08-29 11:17:29 -0400

    Added cmmts

--- jQuery/main.js
+++ jQuery/main.js
@@ -1,16 +1,20 @@
-$(document).ready(function () {
-  $('header button').click(function () {
-    $('form').slideDown();
+$(document).ready(function () { /*jquery inicio*/
+  $('header button').click(function () {/*jquery seleciona botão do header, no evento click abre o form*/
+    $('form').slideDown(); /* ao clicar o form abre para baixo*/
   });
 
-  $('#botao-cancelar').click(function () {
+  $('#botao-cancelar').click(function () {/*jquery acessa botao cancelar evento click*/
+    /*Ao clicar, jquery acessa 'e fecha o form'*/
     $('form').slideUp();
   });
-
+/*acessa o form ,ao enviar previne o reset default */
   $('form').on('submit', function (e) {
     e.preventDefault();
+
     const enderecoDaNovaImagem = $('#endereco-imagem-nova').val();
     const novoItem = $('<li style = "display:none"></li>');
+/**/
+
     $(`<img src="${enderecoDaNovaImagem}"/>`).appendTo(novoItem);
     $(`
               <div class="overlay-imagem-link">
