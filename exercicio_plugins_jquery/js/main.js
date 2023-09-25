efecd87ef9229ae7bf68b5a66a382ec9255dff07
Henrique Bertoli <hbx209@gmail.com>
2023-09-25 09:07:40 -0400

    add mascaras nos campos

--- exercicio_plugins_jquery/js/main.js
+++ exercicio_plugins_jquery/js/main.js
@@ -1,15 +1,9 @@
 $(document).ready(function () {
-  $('#cpf').mask('000.000.000-00', { placeholder: '999.999.999-99' });
+  $('form').css('color', '#0769ad');
+
+  $('#c_p_fisica').mask('000.000.000-00', { placeholder: '000.000.000-00' });
 
   $('#cep').mask('00000-000', { placeholder: '99999-999' });
 
-  $('#tel').mask('(00)00000-0000'),
-    { placeholder: '(99)-99999-8888' },
-    $('form').validate({
-      rules: {
-        nomeCompleto: {
-          required: true,
-        },
-      },
-    });
+  $('#tel').mask('(00)00000-0000', { placeholder: '(99)99999-8888' });
 });
